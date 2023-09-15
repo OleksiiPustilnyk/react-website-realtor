839100188042c7909278f1633ff67d9568521185
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-09-15 21:43:45 +0300

    styled menu, added dropdown menu

--- src/Container/Header/HeaderDefault.tsx
+++ src/Container/Header/HeaderDefault.tsx
@@ -1,22 +1,29 @@
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import Logo from '../../components/Logo/Logo'
 
 import './HeaderDefault.scss'
+import Menu from '../../components/Menu/Menu'
 
 type Props = {}
 
 const HeaderDefault = (props: Props) => {
     return (
         <>
-            <Container className="containerPer" fluid>
-                <Row xs={3}>
-                    <Col xs={{ span: 2, offset: 1 }}>
-                        <Logo />
-                    </Col>
-                    <Col md={{ span: 4, offset: 1 }}>menu</Col>
-                    <Col>View properties</Col>
-                </Row>
-            </Container>
+            <header className="headerDefault">
+                <Container className="containerPer" fluid>
+                    <Row xs={3}>
+                        <Col xs={{ span: 2, offset: 1 }}>
+                            <Logo />
+                        </Col>
+                        <Col md={{ span: 4, offset: 1 }}>
+                            <Menu />
+                        </Col>
+                        <Col>
+                            <button>View properties</button>
+                        </Col>
+                    </Row>
+                </Container>
+            </header>
         </>
     )
 }
