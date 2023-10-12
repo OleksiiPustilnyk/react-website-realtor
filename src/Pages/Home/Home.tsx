9772b60f409bfd6ec51f89af80936b6c569b82ff
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-10-12 17:56:05 +0300

    added 1 bg img in page Home, added title and black box for bg img

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,4 +1,8 @@
+import { Col, Container, Row } from 'react-bootstrap'
 import HeaderDefault from '../../Container/Header/HeaderDefault'
+import ImageHomeBG1 from '../../assets/images/img-bg-home-1.jpeg'
+
+import './Home.scss'
 
 type Props = {}
 
@@ -6,6 +10,87 @@ const Home = (props: Props) => {
     return (
         <>
             <HeaderDefault />
+            <main className="mainHome">
+                <Container fluid="xxl">
+                    <Row xs={1} md={2} className="container-title">
+                        <Col className="title">
+                            <div className="title-text">
+                                <h1 className="title-qoute">
+                                    “Architecture should speak of its time and
+                                    place, but yearn for timelessness.”
+                                </h1>
+                                <p className="title-author">– Frank Gehry</p>
+                            </div>
+                        </Col>
+                        <Col className="title2">
+                            <div className="title2-text">
+                                <h5 className="title2-heading">
+                                    Bibendum morbi ac et porttitor.
+                                </h5>
+                                <p className="title2-paragraph">
+                                    Ut sed semper velit commodo, tortor
+                                    malesuada. Malesuada nisl pharetra a sit
+                                    augue nec bibendum morbi. Ac et, porttitor
+                                    est elit mauris scelerisque et lobortis
+                                    eget.
+                                </p>
+                                <button className="title2-btn">
+                                    View properties
+                                </button>
+                            </div>
+                        </Col>
+                    </Row>
+                    <div className="home-bg">
+                        <img src={ImageHomeBG1} alt="" className="img-bg" />
+                        <Row>
+                            <Col md={7}>
+                                <div className="container-title">
+                                    <div className="bg-black-box">
+                                        <div className="black-box-city">
+                                            San Francisco, United States
+                                        </div>
+                                        <h2 className="black-box-title">
+                                            Cozy beach house
+                                        </h2>
+                                        <span className="black-box-icon-line"></span>
+                                        <div className="black-box-col">
+                                            <Row>
+                                                <Col sm={3}>
+                                                    <h3 className="black-box-title">
+                                                        Bedrooms
+                                                    </h3>
+                                                    <div className="black-box-parameters">
+                                                        3
+                                                    </div>
+                                                    <span className="black-box-icon"></span>
+                                                </Col>
+                                                <Col sm={3}>
+                                                    <h3 className="black-box-title">
+                                                        Bathrooms
+                                                    </h3>
+                                                    <div className="black-box-parameters">
+                                                        2
+                                                    </div>
+                                                    <span className="black-box-icon"></span>
+                                                </Col>
+                                                <Col sm={6}>
+                                                    <h3 className="black-box-title">
+                                                        Area
+                                                    </h3>
+                                                    <div className="black-box-parameters">
+                                                        2300<sup>2</sup>
+                                                    </div>
+                                                    <span className="black-box-icon"></span>
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                    </div>
+                </Container>
+            </main>
         </>
     )
 }
